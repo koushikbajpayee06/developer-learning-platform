f58e30a31f812dd7b3b293caef09154c0f6b8cc0
Koushik <Koushikbajpayee06@gmail.com>
2026-03-16 01:33:25 +0530

    route added for educator

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,17 @@ import CourseDetails from './pages/student/CourseDetails'
 import MyEnrollments from './pages/student/MyEnrollments'
 import Player from './pages/student/Player'
 import Loading from './components/student/Loading'
+import Educator from './pages/educator/Educator'
+import Dashboard from './pages/educator/Dashboard'
+import AddCourse from './pages/educator/AddCourse'
+import StudentsEnrolled from './pages/educator/StudentsEnrolled'
+import MyCourses from './pages/educator/MyCourses'
 
 const App = () => {
   return (
     <div>
       <Routes>
+        {/* Route for Students */}
         <Route path='/' element={<Home/>}/>
         <Route path='/course-list' element={<CoursesList/>}/>
         <Route path='/course-list/:input' element={<CoursesList/>}/>
@@ -18,6 +24,13 @@ const App = () => {
         <Route path='/my-enrollments' element={<MyEnrollments/>}/>
         <Route path='/player/:courseId' element={<Player/>}/>
         <Route path='/loading/:path' element={<Loading/>}/>
+        {/* Routes for Educators */}
+        <Route path='/educator' element={<Educator/>}>
+          <Route path='educator' element={<Dashboard/>}/>
+          <Route path='add-course' element={<AddCourse/>}/>
+          <Route path='my-courses' element={<MyCourses/>}/>
+          <Route path='student-enrolled' element={<StudentsEnrolled/>}/>
+        </Route>
       </Routes>
     </div>
   )
