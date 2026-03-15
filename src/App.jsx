b87fb123c9c06b7cce607d1345bc437f56d4eb1c
Koushik <Koushikbajpayee06@gmail.com>
2026-03-15 23:45:59 +0530

    feat: add AppContext provider and initial routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Home from './pages/student/Home'
+import { Routes, Route } from 'react-router-dom'
+import CoursesList from './pages/student/CoursesList'
+import CourseDetails from './pages/student/CourseDetails'
+import MyEnrollments from './pages/student/MyEnrollments'
+import Player from './pages/student/Player'
+import Loading from './components/student/Loading'
 
 const App = () => {
   return (
     <div>
-      <h1  className="text-3xl font-bold underline">Koushik</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/course-list' element={<CoursesList/>}/>
+        <Route path='/course-list/:input' element={<CoursesList/>}/>
+        <Route path='/course/:id' element={<CourseDetails/>}/>
+        <Route path='/my-enrollments' element={<MyEnrollments/>}/>
+        <Route path='/player/:courseId' element={<Player/>}/>
+        <Route path='/loading/:path' element={<Loading/>}/>
+      </Routes>
     </div>
   )
 }
