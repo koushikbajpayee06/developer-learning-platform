6c7b6f1a264af09264a28dc5f975d903cf1470b7
Koushik <Koushikbajpayee06@gmail.com>
2026-03-27 23:06:47 +0530

    player page created colpleately

--- src/components/student/Rating.jsx
+++ src/components/student/Rating.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Rating = () => {
+const Rating = ({initialRating, onRate}) => {
+  const [rating, setRating] = useState(initialRating || 0)
+  const handleRating = (value )=>{
+    setRating(value);
+    if(onRate) onRate(value)
+  }
+  useEffect(()=>{
+    if(initialRating){
+      setRating(initialRating)
+    }
+  },[initialRating])
   return (
     <div>
-      <h1>Rating</h1>
+      {Array.from({ length: 5 }, (_, i) => {
+        const starValue = i+1;
+        return (
+        <span onClick={()=>handleRating(starValue)} key={i} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue <=rating 
+          ? 'text-yellow-500' : 'text-gray-400'
+        }`}>
+          &#9733;
+        </span>
+        )
+        
+      })}
     </div>
   )
 }
--- src/pages/student/Player.jsx
+++ src/pages/student/Player.jsx
@@ -3,7 +3,9 @@ import { AppContext } from "../../context/AppContext";
 import { useParams } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
-
+import YouTube from 'react-youtube'
+import Footer from "../../components/student/Footer";
+import Rating from "../../components/student/Rating";
 
 const Player = () => {
   const { enrolledCourses, calculateCourseTime } = useContext(AppContext);
@@ -26,7 +28,7 @@ const Player = () => {
 
   useEffect(() => {
     getCourseData();
-  }, []);
+  }, [getCourseData]);
   return (
     <>
       <div className="p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-2 md:px-36">
@@ -67,7 +69,7 @@ const Player = () => {
                       {chapter.chapterContent.map((lecture, idx) => (
                         <li key={idx} className="flex items-start gap-2 py-1">
                           <img
-                            src={false  ? assets.blue_tick_icon : assets.play_icon}
+                            src={false? assets.blue_tick_icon : assets.play_icon}
                             alt="play_icon"
                             className="w-4 h-4 mt-1"
                           />
@@ -80,7 +82,7 @@ const Player = () => {
                                 <p
                                   onClick={() =>
                                     setPlayerData({
-                                      ...lecture, chapter: index+1, lecture:i+1
+                                      ...lecture, chapter: index+1, lecture: idx+1
                                     })
                                   }
                                   className="cursor-pointer text-blue-600"
@@ -103,10 +105,30 @@ const Player = () => {
                 </div>
               ))}
           </div>
+          <div className="flex items-center gap-2 py-3 mt-10">
+            <h1 className="text-xl font-bold">Rate this Course:</h1>
+            <Rating initialRating={0}/>
+          </div>
         </div>
         {/* Right column */}
-        <div></div>
+        <div className="md:mt-10">
+          {
+            playerData ? (
+              <div>
+                <YouTube videoId={playerData.lectureUrl.split('/').pop()}  iframeClassName='w-full aspect-video'/>
+                <div className="flex justify-between items-center mt-1">
+                  <p>{playerData.chapter}.{playerData.lecture} {playerData.lectureTitle}</p>
+                  <button className="text-blue-600">{false ? "Compleated" :"Mark Complete"}</button>
+                </div>
+              </div>
+            ) :
+            <img src={courseData ? courseData.courseThumbnail : ''} alt="" />
+          }
+          
+
+        </div>
       </div>
+      <Footer/>
     </>
   );
 };
